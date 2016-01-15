c8f0eda624c826ced724651a5c012853750b7a25
Frazier Mork <fraziermork@gmail.com>
2016-01-15 10:45:52 -0800

    got it to rewrite everything if a replacement is triggered but it also rewrites all other table entries

--- js/dataapp.js
+++ js/dataapp.js
@@ -7,12 +7,14 @@ indexHeader.style.backgroundSize = "960px 540px"
 //global variables
 var totalLbsForAllShops = 0;
 var newShops = 0;
+var locationsObjectArray = [];
 var locations = [];
 locations[0] = ['Capitol Hill', 3.2, .4, 32, 48, 'capitolHill'];
 locations[1] = ['Pike Place Market', 1.2, 3.7, 14, 55, 'pikePlace'];
 locations[2] = ['Seattle Public Library', 2.6, .2, 49, 75, 'seattlePubLib'];
 locations[3] = ['Sea Tac', 1.1, 2.7, 68, 124, 'seaTac'];
 locations[4] = ['Website', 0, 6.7, 3, 6, 'website'];
+
 
 //constructor function for a new shop location
 function shopLocation (listInput){
@@ -45,7 +47,7 @@ function shopLocation (listInput){
   //add a log to the body of the page for that store, update the summary table
   this.renderActivityLog = function(){
     //set up summary table
-    var summaryTableEl = document.getElementById('summaryTable'); //the summary table
+    var summaryTableEl = document.getElementById('summaryTable'); //grab the summary table
     var summaryRowEl = document.createElement('tr'); //the row we are writing to
     summaryTableEl.appendChild(summaryRowEl);
     var summaryRowFirstDatumEl = document.createElement('td'); //the first element of the row with the name
@@ -57,7 +59,8 @@ function shopLocation (listInput){
     var containerEl = document.createElement('div'); //the div that contain's this location's log
     containerEl.className = 'logContainer';
 
-    document.body.appendChild(containerEl);
+    var activityLogTableEl = document.getElementById('activityLogTable');
+    activityLogTableEl.appendChild(containerEl);
     var titleEl = document.createElement('h3'); // the title of this location's log
     titleEl.textContent = this.locName + ': ';
     containerEl.appendChild(titleEl);
@@ -100,7 +103,7 @@ function shopLocation (listInput){
   }
 }
 
-//set up summary table
+//set up summary table head
 function summarySetUp(){
   var hours = ['6:00AM', '7:00AM','8:00AM', '9:00AM', '10:00AM','11:00AM', '12 NOON', '1:00PM','2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM'];
   var summaryHeadRowEl = document.createElement('tr');
@@ -118,20 +121,28 @@ function summarySetUp(){
 }
 summarySetUp();
 
+
+
 //generate location summaries
 function writeActivityLog(myLocations){
   for (var i = 0; i < myLocations.length; i++) {
     var locationObject = new shopLocation(myLocations[i]);
-    myLocations[i] = locationObject;
+    locationsObjectArray[i] = locationObject;
+    // myLocations[i] = locationObject; //from old code when locations was being rewritten to contain objects instead
     totalLbsForAllShops += locationObject.renderActivityLog();
   }
   //write the total amount sold in the bottom of the summary
   var summaryTotal = document.getElementById('summaryTotal');
-  summaryTotal.textContent = 'The total pounds consumed by all shops today is ' + Number(totalLbsForAllShops.toFixed(1)) + " lbs.";
-  console.log('The total for all shops today is:' + totalLbsForAllShops);
-  console.dir(myLocations);
+  summaryTotal.textContent = '(Running in writeActivityLog) The total pounds consumed by all shops today is ' + Number(totalLbsForAllShops.toFixed(1)) + " lbs.";
+  // console.log('The total for all shops today is:' + totalLbsForAllShops);
+  // console.log('(Running inside myActivityLog)the locations array is ');
+  // console.dir(locations);
+  // console.log('(Running inside myActivityLog) the locationsObjectArray is ');
+  // console.dir(locationsObjectArray);
 }
 writeActivityLog(locations);
+console.log('(Running just after writeActivityLog) This locations array is ');
+console.dir(locations);
 
 
 
@@ -139,10 +150,9 @@ writeActivityLog(locations);
 
 //executes on click of submit button for form
 //submits a new store based on information typed into the form at the top, adds log, updates summary
-
-
 function onSubmit(event){
   var rewriteFlag = false;
+  var rewriteLocation = locations.length;
   console.log(event);
   event.preventDefault();
   if (!event.target.newLocationName.value || !event.target.newLocationMin.value || !event.target.newLocationMax.value || !event.target.newLocationCups.value || !event.target.newLocationLbs.value ){
@@ -153,27 +163,71 @@ function onSubmit(event){
   var newMax = +event.target.newLocationMax.value;
   var newCups = +event.target.newLocationCups.value;
   var newLbs = +event.target.newLocationLbs.value;
+
+  console.log('newName is ' + newName);
+  console.log('newMin is ' + newMin);
+  console.log('newMax is ' + newMax);
+  console.log('newCups is ' + newCups);
+  console.log('newLbs is ' + newLbs);
+
   //Check to see if the new shop is named the same as an old one
   for(var i = 0; i < locations.length; i++){
-    if (newName === locations[i].locName){
+    if (newName.toLowerCase() === locations[i][0].toLowerCase()){
       rewriteFlag = true;
-      console.log(rewriteFlag);
+      rewriteLocation = i;
+      console.log('rewriteFlag is ' + rewriteFlag);
     }
   }
+  //update the locations array
+  locations[rewriteLocation] = [newName, newCups, newLbs, newMin, newMax];
+  console.log('(Running inside of onSubmit) the new locations array is ');
+  console.log(locations);
 
-  newLocation = new shopLocation([newName, newCups, newLbs, newMin, newMax]);
-  console.log(newLocation);
+
+  //if the name is the same as an existing object
+  if (rewriteFlag){
+    //update the object in the location array with the new object
+    console.log('(running inside onsubmit) rewrite triggered, the locations  array before is');
+    console.log(locations);
+    console.log('(running inside onsubmit) rewrite triggered, the locationsObjectArray to be erased is');
+    console.dir(locationsObjectArray);
+    //reinitialize the object array
+    locationsObjectArray = [];
+    console.log('(running inside onsubmit) rewrite triggered, the locationsObjectArray after erasing is');
+    console.log(locationsObjectArray);
+    // grab summarytable and delete all content
+    var summaryTableEl = document.getElementById('summaryTable');
+    summaryTableEl.innerHTML = '';
+    //grab the tableActivityLog and delete all content
+    var activityLogTableEl = document.getElementById('activityLogTable');
+    activityLogTableEl.innerHTML = '';
+    //reinitialize the summary table
+    summarySetUp();
+    //write the whole table over again
+    writeActivityLog(locations);
+
+  //if we are writing a new location
+  } else {
+    newLocation = new shopLocation([newName, newCups, newLbs, newMin, newMax]);
+    console.log('(running inside onsubmit) the new location that was built is');
+    console.log(newLocation);
+    //write the activity log for new entry, return total daily lbs for that location and add to total for all shops
+    totalLbsForAllShops += newLocation.renderActivityLog();
+    //update the summary to account for new entry
+    var summaryTotal = document.getElementById('summaryTotal');
+    summaryTotal.textContent = 'The total pounds consumed by all shops today is ' + Number(totalLbsForAllShops.toFixed(1)) + " lbs.";
+    locationsObjectArray.push(newLocation);
+    console.log('(running inside onsubmit) a new location was made, the locations array is ');
+    console.log(locations);
+    console.log('(running inside onsubmit) a new location was made, the locationsObjectArray is ');
+    console.dir(locationsObjectArray);
+  }
 
 
-
-
-  //write the activity log for new entry
-  totalLbsForAllShops += newLocation.renderActivityLog();
-  //update the summary to account for new entry
-  var summaryTotal = document.getElementById('summaryTotal');
-  summaryTotal.textContent = 'The total pounds consumed by all shops today is ' + Number(totalLbsForAllShops.toFixed(1)) + " lbs.";
-  locations.push(newLocation);
 }
 
 var storage = document.getElementById("submitForm");
 storage.addEventListener('submit', onSubmit, true);
+
+
+//figure out how to write things in place -- can write fast one to delete all and rebuild, slow one with rewrite method
